fe14fff11b0d1bd4582c41d4285eba0d53ecf892
Henrik Henriksson <hx@hx.ax>
2017-09-22 19:20:14 +0200

    Add license and contact information directly to source files

--- smallvec.h
+++ smallvec.h
@@ -1,3 +1,23 @@
+////////////////////////////////////////////////////////////////////////////
+// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                            //
+// Version 2, December 2004                                               //
+//                                                                        //
+//   Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>                     //
+//                                                                        //
+//   Everyone is permitted to copy and distribute verbatim or modified    //
+//   copies of this license document, and changing it is allowed as long  //
+//   as the name is changed.                                              //
+//                                                                        //
+//   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                          //
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION      //
+//                                                                        //
+//   0. You just DO WHAT THE FUCK YOU WANT TO.                            //
+////////////////////////////////////////////////////////////////////////////
+
+// https://github.com/henrixh/smallvec
+// Maintainer: Henrik Henriksson <hx@hx.ax>
+// Authors: Henrik Henriksson <hx@hx.ax> & Mattias Hammar <mahavilan@gmail.com>
+
 #ifndef SMALLVEC_H
 #define SMALLVEC_H
 
