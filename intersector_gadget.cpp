7a10f2dd95e0edf4f477369e88e19d1c8970e97f
Henrik Henriksson <hx@hx.ax>
2017-09-22 20:06:14 +0200

    Reindent intersector_gadget.cpp

--- intersector_gadget.cpp
+++ intersector_gadget.cpp
@@ -100,182 +100,182 @@ namespace gadget {
     return scale(v0 + v1 + v2, 1.0/3.0);
   }
 
-aabb::aabb(const vec3 & max_v, const vec3 & min_v) {
-  this->max_v = max_v;
-  this->min_v = min_v;
-}
+  aabb::aabb(const vec3 & max_v, const vec3 & min_v) {
+    this->max_v = max_v;
+    this->min_v = min_v;
+  }
 
-aabb::aabb(const triangle & tri) {
-  max_v = max(tri.v0, max(tri.v1, tri.v2));
-  min_v = min(tri.v0, min(tri.v1, tri.v2));
-}
+  aabb::aabb(const triangle & tri) {
+    max_v = max(tri.v0, max(tri.v1, tri.v2));
+    min_v = min(tri.v0, min(tri.v1, tri.v2));
+  }
 
   aabb::aabb(const std::vector<triangle> & tris) {
-  if (!tris.size()) {
-    return;
+    if (!tris.size()) {
+      return;
+    }
+
+    aabb res {tris[0]};
+    for (size_t i = 1; i < tris.size(); ++i) {
+      res.merge_with({tris[i]});
+    }
+
+    max_v = res.max_v;
+    min_v = res.min_v;
   }
 
-  aabb res {tris[0]};
-  for (size_t i = 1; i < tris.size(); ++i) {
-    res.merge_with({tris[i]});
+  void aabb::merge_with(const aabb & other) {
+    max_v = max(max_v, other.max_v);
+    min_v = min(min_v, other.min_v);
   }
 
-  max_v = res.max_v;
-  min_v = res.min_v;
-}
 
-void aabb::merge_with(const aabb & other) {
-  max_v = max(max_v, other.max_v);
-  min_v = min(min_v, other.min_v);
-}
-
-
-axis_t aabb::longest_axis() const {
-  vec3 diff = max_v - min_v;
-  if (diff.x() >= diff.y() && diff.x() >= diff.z()) return X_AXIS;
-  if (diff.y() >= diff.z() && diff.y() >= diff.x()) return Y_AXIS;
-  if (diff.z() >= diff.x() && diff.z() >= diff.y()) return Z_AXIS;
-  return X_AXIS;
-}
-
-fp aabb::mid_point_on_longest_axis() const {
-  axis_t axis = longest_axis();
-
-  if (axis == X_AXIS) {
-    return min_v.x() + 0.5*(max_v.x() - min_v.x());
-  }
-  if (axis == Y_AXIS) {
-    return min_v.y() + 0.5*(max_v.y() - min_v.y());
-  }
-  if (axis == Z_AXIS) {
-    return min_v.z() + 0.5*(max_v.z() - min_v.z());
+  axis_t aabb::longest_axis() const {
+    vec3 diff = max_v - min_v;
+    if (diff.x() >= diff.y() && diff.x() >= diff.z()) return X_AXIS;
+    if (diff.y() >= diff.z() && diff.y() >= diff.x()) return Y_AXIS;
+    if (diff.z() >= diff.x() && diff.z() >= diff.y()) return Z_AXIS;
+    return X_AXIS;
   }
 
-  // Should never happen.
-  return infinity;
-}
+  fp aabb::mid_point_on_longest_axis() const {
+    axis_t axis = longest_axis();
+
+    if (axis == X_AXIS) {
+      return min_v.x() + 0.5*(max_v.x() - min_v.x());
+    }
+    if (axis == Y_AXIS) {
+      return min_v.y() + 0.5*(max_v.y() - min_v.y());
+    }
+    if (axis == Z_AXIS) {
+      return min_v.z() + 0.5*(max_v.z() - min_v.z());
+    }
+
+    // Should never happen.
+    return infinity;
+  }
 
   intersector::intersector(std::vector<triangle> triangles){
     std::cout << "Creating bvh tree with " << triangles.size() << " triangles" << std::endl;
-  bvh_node root {triangles, 0, 0, 0};
-  nodes.push_back(root);
-  build_tree(0, triangles);
-  std::cout << "Created bvh tree with " << nodes.size() << " nodes" << std::endl;
-}
-
-fp intersector::get_point(axis_t a, vec3 v){
-  if (a == X_AXIS){
-    return v.x();
-  }else if (a == Y_AXIS){
-    return v.y();
-  }else{
-    return v.z();
+    bvh_node root {triangles, 0, 0, 0};
+    nodes.push_back(root);
+    build_tree(0, triangles);
+    std::cout << "Created bvh tree with " << nodes.size() << " nodes" << std::endl;
   }
-}
+
+  fp intersector::get_point(axis_t a, vec3 v){
+    if (a == X_AXIS){
+      return v.x();
+    }else if (a == Y_AXIS){
+      return v.y();
+    }else{
+      return v.z();
+    }
+  }
 
   void intersector::build_tree(uint32_t node_index, std::vector<triangle> & t){
-  bvh_node node = nodes[node_index];
-  if(t.size() <= threshold){
-    node.tri_index = tris.size();
-    tris.push_back(t);
-    nodes[node_index] = node;
-    return;
-  }
-
-  axis_t axis = node.bound.longest_axis();
-  float mid_point = node.bound.mid_point_on_longest_axis();
-
-
-  std::vector<triangle> left_vec;
-  std::vector<triangle> right_vec;
-
-  for (auto && a : t) {
-    if (get_point(axis, a.center_point()) < mid_point) {
-      left_vec.push_back(a);
-    } else {
-      right_vec.push_back(a);
+    bvh_node node = nodes[node_index];
+    if(t.size() <= threshold){
+      node.tri_index = tris.size();
+      tris.push_back(t);
+      nodes[node_index] = node;
+      return;
     }
+
+    axis_t axis = node.bound.longest_axis();
+    float mid_point = node.bound.mid_point_on_longest_axis();
+
+
+    std::vector<triangle> left_vec;
+    std::vector<triangle> right_vec;
+
+    for (auto && a : t) {
+      if (get_point(axis, a.center_point()) < mid_point) {
+        left_vec.push_back(a);
+      } else {
+        right_vec.push_back(a);
+      }
+    }
+
+    if (right_vec.empty() || left_vec.empty()) {
+      left_vec.clear(); left_vec.shrink_to_fit();
+      right_vec.clear(); right_vec.shrink_to_fit();
+      std::sort(t.begin(), t.end(), [&](const triangle & a, const triangle & b) -> bool {
+          return get_point(axis, a.center_point()) < get_point(axis, b.center_point());
+        });
+      left_vec = {t.begin(), t.begin() + t.size()/2};
+      right_vec = {t.begin() + t.size()/2, t.end()};
+    }
+
+    t.clear(); t.shrink_to_fit();
+
+    node.left = nodes.size();
+    bvh_node left_node  {left_vec,  0, 0, 0};
+    nodes.push_back(left_node);
+
+    node.right = nodes.size();
+    bvh_node right_node {right_vec, 0, 0, 0};
+    nodes.push_back(right_node);
+
+    build_tree(node.left, left_vec);
+    build_tree(node.right, right_vec);
+    nodes[node_index] = node;
   }
 
-  if (right_vec.empty() || left_vec.empty()) {
-    left_vec.clear(); left_vec.shrink_to_fit();
-    right_vec.clear(); right_vec.shrink_to_fit();
-    std::sort(t.begin(), t.end(), [&](const triangle & a, const triangle & b) -> bool {
-        return get_point(axis, a.center_point()) < get_point(axis, b.center_point());
-      });
-    left_vec = {t.begin(), t.begin() + t.size()/2};
-    right_vec = {t.begin() + t.size()/2, t.end()};
+  intersection intersector::leaf_intersect(const ray & r, triangle tri) const {
+    intersection res;
+    res = full_intersect_triangle(tri, r);
+    res.incoming_ray = r;
+    res.id = tri.id;
+    res.hit = true;
+
+    return res;
   }
 
-  t.clear(); t.shrink_to_fit();
+  intersection intersector::intersect(const ray & r) const {
+    std::stack<uint32_t> frontier;
+    vec3 inv_ray_dir = vec3(1,1,1) / r.dir;
+    frontier.push(0);
 
-  node.left = nodes.size();
-  bvh_node left_node  {left_vec,  0, 0, 0};
-  nodes.push_back(left_node);
-
-  node.right = nodes.size();
-  bvh_node right_node {right_vec, 0, 0, 0};
-  nodes.push_back(right_node);
-
-  build_tree(node.left, left_vec);
-  build_tree(node.right, right_vec);
-  nodes[node_index] = node;
-}
-
-intersection intersector::leaf_intersect(const ray & r, triangle tri) const {
-  intersection res;
-  res = full_intersect_triangle(tri, r);
-  res.incoming_ray = r;
-  res.id = tri.id;
-  res.hit = true;
-
-  return res;
-}
-
-intersection intersector::intersect(const ray & r) const {
-  std::stack<uint32_t> frontier;
-  vec3 inv_ray_dir = vec3(1,1,1) / r.dir;
-  frontier.push(0);
-
-  fp closest_distance = infinity;
-  std::pair<size_t, size_t> closest_object {};
+    fp closest_distance = infinity;
+    std::pair<size_t, size_t> closest_object {};
 
 
-  while (frontier.size()) {
-    uint32_t node_index = frontier.top(); frontier.pop();
-    bvh_node n = nodes[node_index];
+    while (frontier.size()) {
+      uint32_t node_index = frontier.top(); frontier.pop();
+      bvh_node n = nodes[node_index];
 
-    // 0 in any pointer marks a leaf
-    if (n.left == 0 || n.right == 0){
-      if (n.bound.intersect(r.origin, inv_ray_dir) < closest_distance) {
-        for (size_t i { 0 }; i < tris[n.tri_index].size(); ++i) {
-          fp hit = intersect_triangle(tris[n.tri_index][i], r);
-          if (hit < closest_distance) {
-            closest_distance = hit;
-            closest_object = {n.tri_index, i};
+      // 0 in any pointer marks a leaf
+      if (n.left == 0 || n.right == 0){
+        if (n.bound.intersect(r.origin, inv_ray_dir) < closest_distance) {
+          for (size_t i { 0 }; i < tris[n.tri_index].size(); ++i) {
+            fp hit = intersect_triangle(tris[n.tri_index][i], r);
+            if (hit < closest_distance) {
+              closest_distance = hit;
+              closest_object = {n.tri_index, i};
+            }
           }
         }
+        continue;
       }
-      continue;
+
+      fp left_dist = nodes[n.left].bound.intersect(r.origin, inv_ray_dir);
+      fp right_dist = nodes[n.right].bound.intersect(r.origin, inv_ray_dir);
+
+      if (left_dist < right_dist) {
+        if (right_dist < infinity) { frontier.push(n.right); }
+        if (left_dist < infinity) { frontier.push(n.left); }
+      } else {
+        if (left_dist < infinity) { frontier.push(n.left); }
+        if (right_dist < infinity) { frontier.push(n.right); }
+      }
     }
 
-    fp left_dist = nodes[n.left].bound.intersect(r.origin, inv_ray_dir);
-    fp right_dist = nodes[n.right].bound.intersect(r.origin, inv_ray_dir);
-
-    if (left_dist < right_dist) {
-      if (right_dist < infinity) { frontier.push(n.right); }
-      if (left_dist < infinity) { frontier.push(n.left); }
-    } else {
-      if (left_dist < infinity) { frontier.push(n.left); }
-      if (right_dist < infinity) { frontier.push(n.right); }
+    intersection tri_intersection {};
+    if (closest_distance < infinity){
+      tri_intersection = leaf_intersect(r, tris[closest_object.first][closest_object.second]);
     }
+    return tri_intersection;
   }
-
-  intersection tri_intersection {};
-  if (closest_distance < infinity){
-    tri_intersection = leaf_intersect(r, tris[closest_object.first][closest_object.second]);
-  }
-  return tri_intersection;
-}
 
 }
